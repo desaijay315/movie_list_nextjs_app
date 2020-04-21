bb26cf67973575c643eb01ff5a91a77a22861c3e
desaijay315 <desaijay315@gmail.com>
2020-04-21 14:52:16 +0530

    push the project on heroku next js express

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const BASE_URL = 'https://reactherokucustom.herokuapp.com'
+// const BASE_URL = 'https://reactherokucustom.herokuapp.com'
 // const BASE_URL = 'http://localhost:3000'
+const BASE_URL = process.env.BASE_URL;
 const MOVIE_DATA = []
 
 /*
