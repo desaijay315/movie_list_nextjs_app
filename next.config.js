75924311e03f045c318190fc6c5c4d869fe27fa5
desaijay315 <desaijay315@gmail.com>
2020-04-21 14:57:25 +0530

    plugin optimizer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withCss(withSass())
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = withCSS(withSass({
+  webpack(config, {dev}) {
+    if (config.mode === 'production') {
+      if (Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+      }
+    }
+    return config;
+  }
+}));
